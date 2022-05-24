e193fcabe72bc4a7ed8c9dcdf7a733190b3c59f8
kerbong <kerbong@gmail.com>
2022-05-25 02:11:48 +0900

    html version

--- index.js
+++ index.js
@@ -1,30 +1,36 @@
 let randomNum = Math.floor(Math.random() * 100);
 let trial = 0;
-let playerNum;
-
-function askNum() {
-  playerNum = +prompt("숫자를 입력하세요!", "");
-}
+const number_form = document.querySelector("#number-form");
+const number_input = document.querySelector("#number-input");
 
 //답 보여주기
-alert("정답은" + randomNum);
-//반복하기
-while (true) {
+console.log("정답은" + randomNum);
+
+//숫자 입력하면 실행되는 함수
+function enterNum(e) {
+  //새로고침 방지하기
+  e.preventDefault();
+
+  //인풋태그에 입력된 값 가져오기
+  let playerNum = +number_input.value;
+
   //시도한 횟수
   trial += 1;
-  //물어보기
-  askNum();
-  //숫자랑 비교하기
-  // 만약 숫자가 같으면
+
   if (playerNum == randomNum) {
-    console.log("당신이 " + trial + " 번 만에 맞췄습니다!");
-    // 멈추기
-    break;
-  } else if (playerNum > randomNum) {
+    console.log("정답은" + randomNum + "입니다!");
+    console.log("당신이 " + trial + " 번 만에 맞췄습니다!😀");
+
     // 만약 입력한 숫자가 더 크면
+  } else if (playerNum > randomNum) {
     console.log(playerNum + "보다 작은 숫자입니다!");
+
     // 만약 입력한 숫자가 더 작으면
   } else {
     console.log(playerNum + "보다 큰 숫자입니다!");
   }
+
+  number_input.value = "";
 }
+
+number_form.addEventListener("submit", enterNum);
