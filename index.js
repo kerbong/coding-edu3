de3e3ee4d9e5aba60363fa925db0efb0849d0e0b
kerbong <kerbong@gmail.com>
2022-05-25 01:55:23 +0900

    showing trial bigger smaller

--- index.js
+++ index.js
@@ -1,29 +1,30 @@
 let randomNum = Math.floor(Math.random() * 100);
+let trial = 0;
 let playerNum;
-let playTheGame = true;
 
-function askingNum() {
-  playerNum = parseInt(prompt("숫자를 입력하세요!"));
+function askNum() {
+  playerNum = +prompt("숫자를 입력하세요!", "");
 }
 
 //답 보여주기
-console.log(randomNum);
-while (playTheGame) {
-  askingNum();
+alert("정답은" + randomNum);
+//반복하기
+while (true) {
+  //시도한 횟수
+  trial += 1;
+  //물어보기
+  askNum();
+  //숫자랑 비교하기
+  // 만약 숫자가 같으면
   if (playerNum == randomNum) {
-    console.log("당신이 맞췄습니다!");
-    playTheGame = false;
+    console.log("당신이 " + trial + " 번 만에 맞췄습니다!");
+    // 멈추기
+    break;
+  } else if (playerNum > randomNum) {
+    // 만약 입력한 숫자가 더 크면
+    console.log(playerNum + "보다 작은 숫자입니다!");
+    // 만약 입력한 숫자가 더 작으면
+  } else {
+    console.log(playerNum + "보다 큰 숫자입니다!");
   }
 }
-
-// 물어보기
-// 숫자랑 비교하기
-// 만약 숫자가 같으면
-// 멈추기
-// 만약 숫자가 같지 않으면
-// 물어보기
-// 숫자랑 비교하기
-// 물어보기
-// 숫자랑 비교하기
-// 만약 숫자가 같으면
-// 멈추기
